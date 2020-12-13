c3a2151f07b2b7fd02c8f40b9abed6480ae5cc62
Gerrit Begher <gbegher@gmail.com>
2020-12-13 12:46:41 +0100

    [ADD] day 13

--- 2020/util/__init__.py
+++ 2020/util/__init__.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def apply_to(x, fn):
     return fn(x)
 
@@ -22,6 +25,24 @@ def filter(generator, predicate):
     for item in generator:
         if predicate(item):
             yield item
+
+
+def filter_with(predicate):
+    return lambda gen: filter(gen, predicate)
+
+
+def minimum_by(valuation):
+    def minimum_fn(inputs):
+        minimum = None
+
+        for item in inputs:
+            if minimum == None or valuation(item) < valuation(minimum):
+                minimum = item
+
+        return minimum
+
+    return minimum_fn
+
 
 
 def map(fn):
@@ -52,3 +73,22 @@ def split_by(predicate):
 
 def as_ints(data):
     return [int(x) for x in data]
+
+
+def prod(nums):
+    """
+    > Thanks, but no thanks; I was quickly dissuaded from the need for this.
+    lol... what a moron, waisting everyones time. "batteries included", my ass.
+    """
+    return reduce(
+        lambda x, y: x * y,
+        nums,
+        1
+    )
+
+
+def unzip(zipped):
+    return [
+        [x[0] for x in zipped],
+        [x[1] for x in zipped],
+    ]
